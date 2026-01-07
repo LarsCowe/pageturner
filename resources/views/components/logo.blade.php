c3255f465cb0e68b708177923827523640b4e5c3
Lars <lars.cowe@hotmail.com>
2026-01-07 22:14:30 +0100

    design: Refactor UI components to match new theme

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -19,10 +19,10 @@
 
 <a href="{{ route('home') }}" {{ $attributes->merge(['class' => "flex items-center gap-2 font-bold {$textSize} {$class}"]) }}>
     {{-- Book Icon SVG --}}
-    <svg class="{{ $iconSize }} text-indigo-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+    <svg class="{{ $iconSize }} text-amber-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
         <path d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
-    <span class="text-indigo-700">
+    <span class="text-amber-500">
         PageTurner
     </span>
 </a>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-stone-700 bg-stone-900 text-stone-100 focus:border-amber-500 focus:ring-amber-500 rounded-md shadow-sm placeholder-stone-500']) }}>
